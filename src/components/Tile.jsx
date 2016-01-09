09de3071dfc5ad1617c1ae64e9bdbd2d1f6af78a
Johannes Stein <johannesstein@freeze-dev.com>
2016-01-09 15:44:54 +0100

    Tile updates

--- src/components/Tile.jsx
+++ src/components/Tile.jsx
@@ -1,9 +1,21 @@
 import React, {Component} from 'react';
 
 class Tile extends Component {
+  static defaultProps = {
+    width: 64,
+    height: 64,
+  }
+
   render() {
     if (!this.props.type) {
       return null;
+    }
+
+    const style = {
+      x: this.props.x * this.props.width,
+      y: this.props.y * this.props.height,
+      width: this.props.width,
+      height: this.props.height,
     }
 
     return <div className={`tile tile-${this.props.type}`} />;
