aec95c3e52680afaade81c0caa87d875ff936755
Malcolm <crummynz@gmail.com>
2016-01-09 14:03:20 +0100

    Initial Human work, and js -> jsx

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # berlinminigamejam_jan2016
 Berlin Mini Game Jam January 2016
+
+# Populous-like
+Don't directly control people. Plant trees, water, etc, people interact with it all.
+In the dark ages.
+
+Player Actions:
+- Plant trees
+- Plant crops
+- Lightning
+ 
+ Resources:
+ - Wood
+ - Food
+ - People
+
+Gameplay loop:
+- Plant trees
+- People collect wood for houses
+- Plant food
+- People collect food for repopulation
+- People build more houses, grow more people
+- Can only plant time limited number of trees+food, based on PrayerPoints
+- Zap houses if overpopulation occurs
+
+People AI:
+If hungry:
+  If has food:
+    Eat food
+  Else If food available:
+    Get food
+  else:
+    Starve
+Else if homeless:
+  If has wood:
+    Build house
+  Else if wood available:
+    Get wood
+  Else:
+    Freeze
+Else:
+  Pray
---
+++ src/components/Human.jsx
@@ -0,0 +1,58 @@
+import React, {Component} from 'react';
+import GameObject from './GameObject';
+
+class Human extends Component {
+  constructor(props) {
+    super(props);
+    this.needsFood = new Need();
+    this.needsHouse = new Need();
+    this.hasHouse = false;
+    this.food = new Resource();
+    this.wood = new Resource();
+  }
+  
+  tick() {
+    this.needsFood.tick();
+    this.needsHouse.tick();
+    if ()
+  }
+  
+  render() {
+    return <GameObject />
+  }
+}
+
+class Need {
+  constructor() {
+    this.min = 0;
+    this.max = 100;
+    this.value = 20;
+    this.importantLevel = 50;
+    this.criticalLevel = 85;
+  }
+  
+  tick() {
+    this.value++;
+    if (this.value > this.max) {
+      this.value = this.max);
+    }
+  }
+  
+  isImportant() {
+    return this.value >= this.importantLevel && this.value < this.criticalLevel;
+  }
+  
+  isCritical() {
+    return this.value >= this.criticalLevel;
+  }
+}
+
+class Resource {
+  constructor() {
+    this.min = 0;
+    this.max = 10;
+    this.value = 0;
+  }
+}
+
+export default Human;
