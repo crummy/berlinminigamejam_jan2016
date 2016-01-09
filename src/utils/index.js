5a1a00e336894f3919db9aa263d67683a964d4d8
Malcolm <crummynz@gmail.com>
2016-01-09 17:49:49 +0100

    Fleshing out world methods

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,6 @@
 import tileToPixel from './tileToPixel';
 import pixelToTile from './pixelToTile';
 import distanceBetween from './distance';
+import randomInt from './randomInt';
 
-export default {tileToPixel, pixelToTile, distanceBetween };
+export default {tileToPixel, pixelToTile, distanceBetween, randomInt };
---
+++ src/utils/randomInt.js
@@ -0,0 +1,4 @@
+// random int between a and b inclusive
+export default function(a, b) {
+  return Math.floor((Math.random() * b) + a);
+}
