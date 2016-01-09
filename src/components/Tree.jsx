b5633e7ced42926f22305a2f33394d129c132028
Johannes Stein <johannesstein@freeze-dev.com>
2016-01-09 14:13:08 +0100

    Tree stub

---
+++ src/components/Tree.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import GameObject from '../GameObject';
+
+class Tree extends Component {
+  render() {
+    return <GameObject />;
+  }
+}
+
+export default Tree;
